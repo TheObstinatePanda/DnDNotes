15762b5fa53e681ee0dc4fb92ada6f7eed566a58
TheObstinatePanda <j.m.newman.101@gmail.com>
2024-08-13 23:28:38 -0400

    updating tests

--- app.test.js
+++ app.test.js
@@ -1,43 +1,44 @@
+require('dotenv').config({ })
 const request = require('supertest');
 const app = require('./backend/app'); // Adjust the path if necessary
 
-console.log(app)
+describe('Notes API', () => {
 
-  it('should create a new note', async () => {
-    const res = await request(app)
-      .post('/notes')
-      .send({
-        noteTitle: 'Test Note',
-      });
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toHaveProperty('note');
-  });
+    it('should create a new note', async () => {
+        const res = await request(app)
+            console.log(res.statusCode)
+        .post('/notes')
+        .send({
+            noteTitle: 'Test Note',
+        });
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toHaveProperty('note');
+    });
 
-  describe('Notes API', () => {
     it('should get all notes', async () => {
-      const res = await request(app).get('/notes');
-      expect(res.statusCode).toEqual(200);
-      expect(res.body).toHaveProperty('notes');
-    });
-  
-    it('should get a note by ID', async () => {
-      const res = await request(app).get('/notes/1'); // Adjust the ID as necessary
-      expect(res.statusCode).toEqual(200);
-      expect(res.body).toHaveProperty('note');
-    });
-
-  it('should update a note', async () => {
-    const res = await request(app)
-      .put('/notes/1') // Adjust the ID as necessary
-      .send({
-        noteTitle: 'Updated Note',
+        const res = await request(app).get('/notes');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('notes');
       });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('note');
-  });
+    
+    it('should get a note by ID', async () => {
+        const res = await request(app).get('/notes/1'); // Adjust the ID as necessary
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('note');
+        });
 
-  it('should delete a note', async () => {
-    const res = await request(app).delete('/notes/1'); // Adjust the ID as necessary
-    expect(res.statusCode).toEqual(204);
-  });
+    it('should update a note', async () => {
+        const res = await request(app)
+        .put('/notes/1') // Adjust the ID as necessary
+        .send({
+            noteTitle: 'Updated Note',
+        });
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('note');
+    });
+
+    it('should delete a note', async () => {
+        const res = await request(app).delete('/notes/1'); // Adjust the ID as necessary
+        expect(res.statusCode).toEqual(204);
+    });
 });
